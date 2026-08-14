a3f86e0427cf3d30e5bddc548d8cb721a86fcb72
md-morsalin10 <mdmorsalindeveloper@gmail.com>
2026-08-15 02:55:47 +0600

    login error fixed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -28,5 +28,11 @@ export const auth = betterAuth({
   },
   trustedOrigins: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, "http://localhost:3000"] : ["http://localhost:3000"],
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5000",
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+    },
+  },
   // You can add more Better-Auth configuration here (e.g., social providers)
 });
